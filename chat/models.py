fce9dcffdc86a9a765a1bb1b98477430fbec045b
Yuta Kijima <scwestf95@g.ecc.u-tokyo.ac.jp>
2021-02-23 22:12:04 +0900

    merge

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.utils import timezone
+from user.models import CustomUser
 
 class Room(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
@@ -16,6 +17,6 @@ class Message(models.Model):
         related_name='room_messages',
         on_delete=models.CASCADE
     )
-    name = models.CharField(max_length=50)
+    name = models.ForeignKey(CustomUser,default=1,on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
