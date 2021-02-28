cd55a8f35c26c014530568752c92aed167525ad1
Yuta Kijima <scwestf95@g.ecc.u-tokyo.ac.jp>
2021-02-28 12:10:00 +0900

    finally solved the problem

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,16 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
 from django.urls import reverse
+from user.models import *
+import json
+from django.core import serializers
+from django.forms.models import model_to_dict
+import sys
+from django.views import generic,View
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+
+
 
 def index(request):
     room_list = Room.objects.order_by('-created_at')[:5]
@@ -15,10 +25,11 @@ def index(request):
 def chat(request, room_name):
     messages = Message.objects.filter(room__name=room_name).order_by('-created_at')[:50]
     room = Room.objects.filter(name=room_name)[0]
+    image = CustomUser.objects.get(username=request.user).profile.profile_picture.url
     template = loader.get_template('chat/chat_room.html')
     context = {
         'messages': messages,
-        'room': room
+        'room': room,
     }
     return HttpResponse(template.render(context, request))
 
