4d86649030b92cacb1945c6003310f16eeaf0e28
katok <katoken629@googlemail.com>
2021-03-08 22:36:30 +0900

    pic preview function working fully

--- static/js/post.js
+++ static/js/post.js
@@ -1,5 +1,4 @@
-var inputs = $.makeArray($(".pic_input"));
-var newest_input = inputs[inputs.length - 1];
+var newest_input = $("#newest_pic_input")
 
 
 $(".modal-btn").on("click", function(){
@@ -16,40 +15,42 @@ $(".modal-close").on("click",function(){
 })
 
 $("#picture_add_button").on("click",function(){
+    $("#newest_pic_input").removeAttr("id"); 
     var new_input = $("<input>").attr({"type":"file",
                                        "name":"image",
                                        "accept":"image/*",
-                                       "class":"not_first_pic_input pic_input"})
+                                       "class":"not_first_pic_input pic_input",
+                                       "id":"newest_pic_input"})                                
     $("#act_pic_inputs").append(new_input);
-    newest_input = new_input;
+
 })
 
 $("#pic_preview").on("click",function(){
-    newest_input.click();
-})
-
-
-$(newest_input).on("change",function(){
-    if (newest_input.files && newest_input.files[0]){
-        var post_pic_file = newest_input.files[0];
-        var file_reader = new FileReader();
-        file_reader.onload = function(e){
-            $("#pic_preview").css({"background-image":"url("+e.target.result+")"});
-            $("#pic_preview>h2").hide();
-            setTimeout(function(){
-                var past_pic = $("<div></div>").css({"height":"80%",
-                                                 "width":"50px",
-                                                 "background-image":"url("+e.target.result+")",
-                                                 "background-position":"center center",
-                                                 "background-size":"cover",
-                                                 "border-radius":"10%"})
-                $("#other_pics").append(past_pic);
-                $("#pic_preview").css({"background-image":"none"});
-                $("#pic_preview>h2").show();
-                $("#picture_add_button").click();
-            },2000);
-            
+    $("#newest_pic_input").click();
+    $("#newest_pic_input").on("change",function(){
+        console.log("newestinput changed")
+        if (this.files && this.files[0]){
+            var post_pic_file = this.files[0];
+            var file_reader = new FileReader();
+            file_reader.onload = function(e){
+                $("#pic_preview").css({"background-image":"url("+e.target.result+")"});
+                $("#pic_preview>h2").hide();
+                setTimeout(function(){
+                    var past_pic = $("<div></div>").css({"height":"80%",
+                                                     "width":"50px",
+                                                     "background-image":"url("+e.target.result+")",
+                                                     "background-position":"center center",
+                                                     "background-size":"cover",
+                                                     "border-radius":"10%"})
+                    $("#other_pics").append(past_pic);
+                    $("#pic_preview").css({"background-image":"none"});
+                    $("#pic_preview>h2").show();
+                    $("#picture_add_button").click();
+                },2000);
+                
+            }
+            file_reader.readAsDataURL(post_pic_file)
         }
-        file_reader.readAsDataURL(post_pic_file)
-    }
+    })
 })
+
